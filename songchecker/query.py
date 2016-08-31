e16a7f2ddf4a2cf81fb463e5927c46c98068386f
Martin Ueding <dev@martin-ueding.de>
2016-08-31 21:54:54 +0200

    Catch error when related object cannot be found

--- songchecker/query.py
+++ songchecker/query.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 # Copyright © 2016 Martin Ueding <dev@martin-ueding.de>
 
-import pprint
+import sys
 
+import pprint
 import prettytable
+import sqlalchemy.orm
 
 from songchecker import model
 
@@ -11,24 +13,28 @@ from songchecker import model
 def main(options):
     query = model.session.query(model.Song)
 
-    if options.title is not None:
-        print('Filtering title “{}”.'.format(options.title))
-        query = query.filter(model.Song.title.like('%{}%'.format(options.title)))
+    try:
+        if options.title is not None:
+            print('Filtering title “{}”.'.format(options.title))
+            query = query.filter(model.Song.title.like('%{}%'.format(options.title)))
 
-    if options.artist is not None:
-        print('Filtering artist “{}”.'.format(options.artist))
-        artist_obj = model.session.query(model.Artist).filter(model.Artist.name == options.artist).one()
-        query = query.filter(model.Song.artist == artist_obj)
+        if options.artist is not None:
+            print('Filtering artist “{}”.'.format(options.artist))
+            artist_obj = model.session.query(model.Artist).filter(model.Artist.name == options.artist).one()
+            query = query.filter(model.Song.artist == artist_obj)
 
-    if options.has_video is not None:
-        print('Filtering has_video “{}”.'.format(options.has_video))
-        has_video = True if options.has_video == 'true' else False
-        query = query.filter(model.Song.has_video == has_video)
+        if options.has_video is not None:
+            print('Filtering has_video “{}”.'.format(options.has_video))
+            has_video = True if options.has_video == 'true' else False
+            query = query.filter(model.Song.has_video == has_video)
 
-    if options.genre is not None:
-        print('Filtering genre “{}”.'.format(options.genre))
-        genre_obj = model.session.query(model.Genre).filter(model.Genre.name == options.genre).one()
-        query = query.outerjoin(model.Song.genres).filter(model.Song.genres.contains(genre_obj))
+        if options.genre is not None:
+            print('Filtering genre “{}”.'.format(options.genre))
+            genre_obj = model.session.query(model.Genre).filter(model.Genre.name == options.genre).one()
+            query = query.outerjoin(model.Song.genres).filter(model.Song.genres.contains(genre_obj))
+    except sqlalchemy.orm.exc.NoResultFound:
+        print('Could not find the related object, no results')
+        sys.exit(1)
 
     results = query.all()
 
