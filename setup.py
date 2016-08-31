09f96ef9919fa0f196418243052f229516821316
Martin Ueding <dev@martin-ueding.de>
2016-08-31 23:01:47 +0200

    Add a setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Copyright © 2016 Martin Ueding <dev@martin-ueding.de>
+
+from setuptools import setup, find_packages
+
+setup(
+    author="Martin Ueding",
+    author_email="dev@martin-ueding.de",
+    description="Meta data helper for Ultrastar karaoke software",
+    license="GPL2",
+    classifiers=[
+        "Environment :: Console",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Programming Language :: Python",
+
+    ],
+    name="",
+    packages=find_packages(),
+    entry_points={
+        'songchecker': [
+            'songchecker.__main__:main',
+        ],
+    },
+    install_requires=[
+        'prettytable',
+        'sqlalchemy',
+    ],
+    url="https://github.com/martin-ueding/Ultrastar-Song-Checker",
+    version="0.1",
+)
